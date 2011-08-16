88027eebc238120ecc564f1d75dbe4f7d3850ecb
AoD314 <morozov.andrey.vmk@gmail.com>
2011-08-16 10:50:30 +0400

    added method: mem_copy()

---
+++ include/mlib/memory.hpp
@@ -0,0 +1,16 @@
+
+#ifndef __MEMORY_HPP__
+#define __MEMORY_HPP__
+
+namespace mlib
+{
+	template<typename T>
+	void mem_copy(T * src, T * dst, size_t size)
+	{
+		for (size_t i = 0; i < size; i++)
+			dst[i] = src[i];
+	}
+}
+
+#endif
+

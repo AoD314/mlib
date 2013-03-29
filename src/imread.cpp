007c1316c6055ee4ba6e19c7f1b8f92422a5503a
AoD314 <morozov.andrey.vmk@gmail.com>
2013-03-29 20:06:14 +0400

    added 2 not implemented methods

---
+++ src/imread.cpp
@@ -0,0 +1,11 @@
+
+#include "mlib/imread.hpp"
+#include "mlib/exception.hpp"
+
+namespace mlib
+{
+    Image imread (std::string /*filename*/)
+    {
+        Error("Not implemented");
+    }
+}
---
+++ src/imwrite.cpp
@@ -0,0 +1,11 @@
+
+#include "mlib/imwrite.hpp"
+#include "mlib/exception.hpp"
+
+namespace mlib
+{
+    Image imwrite (std::string /*filename*/, Image /*img*/, unsigned char /*compress*/)
+    {
+        Error("Not implemented");
+    }
+}

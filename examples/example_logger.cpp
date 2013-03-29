94af00ce6c51bd17a2c045d2686c5fe5bfea4dc4
AoD314 <morozov.andrey.vmk@gmail.com>
2013-03-29 20:09:34 +0400

    added info about license in examples

--- examples/example_logger.cpp
+++ examples/example_logger.cpp
@@ -1,13 +1,45 @@
+/*M/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2012 Morozov Andrey (morozov.andrey.vmk@gmail.com)
+//    All Rights Reserved.
+//
+//    Redistribution and use in source and binary forms, with or without
+//    modification, are permitted provided that the following conditions
+//    are met:
+//        1. Redistributions of source code must retain the above copyright
+//           notice, this list of conditions and the following disclaimer.
+//        2. Redistributions in binary form must reproduce the above copyright
+//           notice, this list of conditions and the following disclaimer in
+//           the documentation and/or other materials provided with
+//           the distribution.
+//        3. The name of the author may not be used to endorse or promote
+//           products derived from this software without specific prior
+//           written permission.
+//
+//    THIS SOFTWARE IS PROVIDED BY "AS IS" AND ANY EXPRESS OR
+//    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//    ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+//    THE POSSIBILITY OF SUCH DAMAGE.
+//
+//M*/
 
 #include "mlib/logger.hpp"
 #include "mlib/datetime.hpp"
 
 int main(int /*argc*/, const char ** /*argv*/)
 {
-	mlib::Logger logger("/tmp/my_log_file.txt");
+    mlib::Logger logger("/tmp/my_log_file.txt");
 
-	logger.log(LOG_INFO ("first message" ));
-	logger.log(LOG_DEBUG("new message #1"));
+    logger.log(LOG_INF("first message" ));
+    logger.log(LOG_DBG("new message #1"));
+    logger.log(LOG_ERR("new message #2"));
 
 	return 0;
 }
--- examples/example_proc.cpp
+++ examples/example_proc.cpp
@@ -1,27 +1,62 @@
+/*M/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2012 Morozov Andrey (morozov.andrey.vmk@gmail.com)
+//    All Rights Reserved.
+//
+//    Redistribution and use in source and binary forms, with or without
+//    modification, are permitted provided that the following conditions
+//    are met:
+//        1. Redistributions of source code must retain the above copyright
+//           notice, this list of conditions and the following disclaimer.
+//        2. Redistributions in binary form must reproduce the above copyright
+//           notice, this list of conditions and the following disclaimer in
+//           the documentation and/or other materials provided with
+//           the distribution.
+//        3. The name of the author may not be used to endorse or promote
+//           products derived from this software without specific prior
+//           written permission.
+//
+//    THIS SOFTWARE IS PROVIDED BY "AS IS" AND ANY EXPRESS OR
+//    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//    ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+//    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+//    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+//    THE POSSIBILITY OF SUCH DAMAGE.
+//
+//M*/
 
 #include <iostream>
+#include <limits>
 
 #include <mlib/convert.hpp>
 #include <mlib/processor.hpp>
 
+using namespace mlib;
+
 int main(int /*argc*/, char ** /*argv*/)
 {
     std::cout << "Processor: " << std::endl;
-    std::cout << "MMX   : " << mlib::to_str(mlib::Processor::supportMMX())    << std::endl;
-    std::cout << "SSE   : " << mlib::to_str(mlib::Processor::supportSSE())    << std::endl;
-    std::cout << "SSE2  : " << mlib::to_str(mlib::Processor::supportSSE2())   << std::endl;
-    std::cout << "SSE3  : " << mlib::to_str(mlib::Processor::supportSSE3())   << std::endl;
-    std::cout << "SSSE3 : " << mlib::to_str(mlib::Processor::supportSSSE3())  << std::endl;
-    std::cout << "SSE4.1: " << mlib::to_str(mlib::Processor::supportSSE4_1()) << std::endl;
-    std::cout << "SSE4.2: " << mlib::to_str(mlib::Processor::supportSSE4_2()) << std::endl;
-    std::cout << "AVX   : " << mlib::to_str(mlib::Processor::supportAVX())    << std::endl;
-    std::cout << "AVX2  : " << mlib::to_str(mlib::Processor::supportAVX2())   << std::endl;
+    std::cout << "MMX   : " << to_str(Processor::supportMMX())    << std::endl;
+    std::cout << "SSE   : " << to_str(Processor::supportSSE())    << std::endl;
+    std::cout << "SSE2  : " << to_str(Processor::supportSSE2())   << std::endl;
+    std::cout << "SSE3  : " << to_str(Processor::supportSSE3())   << std::endl;
+    std::cout << "SSSE3 : " << to_str(Processor::supportSSSE3())  << std::endl;
+    std::cout << "SSE4.1: " << to_str(Processor::supportSSE4_1()) << std::endl;
+    std::cout << "SSE4.2: " << to_str(Processor::supportSSE4_2()) << std::endl;
+    std::cout << "AVX   : " << to_str(Processor::supportAVX())    << std::endl;
+    std::cout << "AVX2  : " << to_str(Processor::supportAVX2())   << std::endl;
 
-    std::cout << "FMA   : " << mlib::to_str(mlib::Processor::supportFMA())    << std::endl;
-    std::cout << "AES   : " << mlib::to_str(mlib::Processor::supportAES())    << std::endl;
-    std::cout << "HT    : " << mlib::to_str(mlib::Processor::supportHT())     << std::endl;
-    std::cout << "POPCNT: " << mlib::to_str(mlib::Processor::supportPOPCNT()) << std::endl;
-    std::cout << "RDTSCP: " << mlib::to_str(mlib::Processor::supportRDTSCP()) << std::endl;
+    std::cout << "FMA   : " << to_str(Processor::supportFMA())    << std::endl;
+    std::cout << "AES   : " << to_str(Processor::supportAES())    << std::endl;
+    std::cout << "HT    : " << to_str(Processor::supportHT())     << std::endl;
+    std::cout << "POPCNT: " << to_str(Processor::supportPOPCNT()) << std::endl;
+    std::cout << "RDTSCP: " << to_str(Processor::supportRDTSCP()) << std::endl;
 
     return 0;
 }
+
